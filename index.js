edc987522c17c4a1aa96827b5dd0abdaa26790cd
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-09-01 12:49:58 -0400

    Done.

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ function findMinAndRemoveSorted(array) {
 }
 
 // #merge should merge two sorted arrays to produce one sorted array:
-function merge(firstHalf, secondHalf) {
-  let sorted = [];
-  while (firstHalf.length != 0 && secondHalf.length != 0) {
-    let minfirstHalf = firstHalf[0];
-    let minsecondHalf = secondHalf[0];
+function merge(firstSubarray, secondSubArray) {
+  let sorted = []
+  while (firstSubarray.length != 0 && secondSubArray.length != 0) {
+    let minfirstHalf = firstSubarray[0];
+    let minsecondHalf = secondSubArray[0];
     if (minfirstHalf < minsecondHalf) {
-      sorted.push(minfirstHalf)
-      findMinAndRemoveSorted(firstHalf)
+      sorted.push(findMinAndRemoveSorted(firstSubarray))
     } else {
-      sorted.push(minsecondHalf)
-      findMinAndRemoveSorted(secondHalf)
+      sorted.push(findMinAndRemoveSorted(secondSubArray))
     }
   }
-  return sorted.concat(firstHalf).concat(secondHalf)
+  return sorted.concat(firstSubarray).concat(secondSubArray)
 }
 
 // #mergeSort should sort an unsorted array:
 function mergeSort(array) {
-  let firstHalf = array.slice(0, array.length / 2)
-  let secondHalf = array.slice(array.length / 2, array.length)
+  let midpoint = array.length / 2
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
 
   if (array.length < 2) {
     return array
   } else {
-    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    let sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
   }
+  return sorted
 }
